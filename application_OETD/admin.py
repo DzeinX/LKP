a5ad18d660cd80846bb9338b61ea7ea4a76b1eaa
Пронин Вадим Викторович <proninvv@vogu35.ru>
2023-06-23 13:16:36 +0300

    Initial

--- application_OETD/admin.py
+++ application_OETD/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Category, Criteria, User, Otch_period, Post, Meaning, Inspector, Anketa
+admin.site.register(Category)
+admin.site.register(Criteria)
+admin.site.register(User)
+admin.site.register(Otch_period)
+admin.site.register(Post)
+admin.site.register(Meaning)
+admin.site.register(Inspector)
+admin.site.register(Anketa)
--- application_OETD/models.py
+++ application_OETD/models.py
@@ -1,3 +1,84 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+# Отчетный период
+class Otch_period(models.Model):
+    name = models.CharField('Наименование', max_length=250)
+    activ = models.BooleanField('Активен')
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+# Должность
+class Post(models.Model):
+    name = models.CharField('Наименование', max_length=250)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+# Проверяющий
+class Inspector(models.Model):
+    name = models.CharField('Название', max_length=50)
+    short_name = models.CharField('Сокращение', max_length=10)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+# Пользователь
+class User(models.Model):
+    post_id = models.ManyToManyField(Post)
+    inspector_id = models.ManyToManyField(Inspector)
+
+    def __str__(self):
+        return f'{self.id}'
+
+
+# Категория
+class Category(models.Model):
+    name = models.CharField('Наименование', max_length=250)
+    start = models.DateTimeField('Начало активности')
+    end = models.DateTimeField('Конец активности')
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+# Анкета
+class Anketa(models.Model):
+    post_id = models.ManyToManyField(Post)
+    otch_period_id = models.ManyToManyField(Otch_period)
+    category_id = models.ManyToManyField(Category)
+    name = models.CharField('Наименование', max_length=250)
+    comment = models.TextField('Коментируемая')
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+# Критерий
+class Criteria(models.Model):
+    anketa_id = models.ForeignKey(Anketa, on_delete=models.CASCADE)
+    inspector_id = models.ForeignKey(Inspector, on_delete=models.CASCADE)
+    id_num = models.IntegerField('Порядковый номер', validators=[MinValueValidator(1), MaxValueValidator(100)])
+    name = models.CharField('Наименование', max_length=250)
+    descriptions = models.TextField('Описание')
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+# Значение (затычка)
+class Meaning(models.Model):
+    criteria_id = models.ForeignKey(Criteria, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField('Наименование', max_length=250)
+    comment = models.TextField('Коментарий')
+    block = models.BooleanField('Блокировка')
+
+    def __str__(self):
+        return f'{self.name}'
