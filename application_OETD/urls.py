8299b2165cb01bf891c0e97981e7ea03e7958823
4IPE <all0b0y@yandex.ru>
2023-06-25 15:05:10 +0300

    add urls and views

--- application_OETD/urls.py
+++ application_OETD/urls.py
@@ -5,5 +5,18 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.main, name='main')
+    path('', views.main, name='main'),
+    path('', views.main, name='home'),
+    path('', views.main, name='baseTemplate'),
+    path('', views.main, name='app'),
+    path('', views.main, name='loginPage'),
+    path('', views.main, name='create'),
+    path('', views.main, name='critery'),
+    path('', views.main, name='criteryCategory'),
+    path('', views.main, name='edit'),
+    path('', views.main, name='editAdd'),
+    path('', views.main, name='efficiency'),
+    path('', views.main, name='portfolio'),
+    path('', views.main, name='report'),
+    path('', views.main, name='show')
 ]
--- application_OETD/views.py
+++ application_OETD/views.py
@@ -6,3 +6,61 @@ from django.shortcuts import render
 def main(request):
     context = {}
     return render(request, 'lkp_logic/main.html', context)
+
+@login_required
+def home(request):
+    context = {}
+    return render(request, 'templates/home.html', context)
+
+@login_required
+def baseTemplate(request):
+    context = {}
+    return render(request, 'templates/Base_template.html', context)
+
+@login_required
+def app(request):
+    context = {}
+    return render(request, 'templates/app.html', context)
+
+@login_required
+def loginPage(request):
+    context = {}
+    return render(request, 'templates/registration/login_page.html', context)
+
+@login_required
+def create(request):
+    context = {}
+    return render(request, 'templates/lkp_logic/create.html', context)
+@login_required
+def critery(request):
+    context = {}
+    return render(request, 'templates/lkp_logic/critery.html', context)
+@login_required
+def criteryCategory(request):
+    context = {}
+    return render(request, 'templates/lkp_logic/critery_category.html', context)
+@login_required
+def edit(request):
+    context = {}
+    return render(request, 'templates/lkp_logic/edit.html', context)
+@login_required
+def editAdd(request):
+    context = {}
+    return render(request, 'templates/lkp_logic/edit-add.html', context)
+@login_required
+def efficiency(request):
+    context = {}
+    return render(request, 'templates/lkp_logic/efficiency.html', context)
+@login_required
+def portfolio(request):
+    context = {}
+    return render(request, 'templates/lkp_logic/portfolio.html', context)
+@login_required
+def report(request):
+    context = {}
+    return render(request, 'templates/lkp_logic/report.html', context)
+
+@login_required
+def show(request):
+    context = {}
+    return render(request, 'templates/lkp_logic/show.html', context)
