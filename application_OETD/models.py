aad1144b47fcc8e68239607ee49b7d91887b84da
4IPE <all0b0y@yandex.ru>
2023-06-29 01:13:59 +0300

    fix camelCase and templates error deleted

--- application_OETD/models.py
+++ application_OETD/models.py
@@ -5,9 +5,9 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 # Отчетный период
 class Permissions(models.Model):
     key = models.CharField('Значение', max_length=250)
-    table_name = models.CharField('Название таблицы', max_length=250,null=True)
-    created_at = models.DateTimeField('Время создание ',blank=True)
-    updated_at = models.DateTimeField('Время изменения',blank=True)
+    table_name = models.CharField('Название таблицы', max_length=250, null=True)
+    created_at = models.DateTimeField('Время создание ', blank=True)
+    updated_at = models.DateTimeField('Время изменения', blank=True)
 
     def __str__(self):
         return f'{self.table_name}'
@@ -32,17 +32,18 @@ class Menus(models.Model):
         return f'{self.name}'
 
 
-class Menuitems(models.Model):
+class MenuItems(models.Model):
     menu_id = models.ForeignKey(Menus, on_delete=models.CASCADE)
     title = models.CharField('Заголовок', max_length=250)
     url = models.CharField('urls', max_length=250)
     target = models.CharField('Цель', max_length=250)
-    icon_class = models.CharField('Класс иконки', max_length=250,null=True)
-    color = models.CharField('Цвет', max_length=250,null=True)
-    parent_id = models.IntegerField('Родительский id', validators=[MinValueValidator(1), MaxValueValidator(100)],null=True)
+    icon_class = models.CharField('Класс иконки', max_length=250, null=True)
+    color = models.CharField('Цвет', max_length=250, null=True)
+    parent_id = models.IntegerField('Родительский id', validators=[MinValueValidator(1), MaxValueValidator(100)],
+                                    null=True)
     order = models.IntegerField('order', validators=[MinValueValidator(1), MaxValueValidator(100)])
-    route = models.CharField('Route', max_length=250,null=True)
-    parameters = models.TextField('Параметры',null=True)
+    route = models.CharField('Route', max_length=250, null=True)
+    parameters = models.TextField('Параметры', null=True)
     created_at = models.DateTimeField('Время создание ', blank=True)
     updated_at = models.DateTimeField('Время изменения', blank=True)
 
@@ -85,7 +86,7 @@ class Fields(models.Model):
     name = models.TextField('Наименование', max_length=250)
     description = models.TextField('Описание', max_length=250)
     form_id = models.IntegerField("Форма", validators=[MinValueValidator(1), MaxValueValidator(100)])
-    inspector_id = models.ForeignKey(Inspectors, on_delete=models.SET_NULL,null=True)
+    inspector_id = models.ForeignKey(Inspectors, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return f'{self.name}'
@@ -94,11 +95,9 @@ class Fields(models.Model):
 class Forms(models.Model):
     created_at = models.DateTimeField('Время создание ', blank=True)
     updated_at = models.DateTimeField('Время изменения', blank=True)
-    reporting_period_id = models.ForeignKey(ReportingPeriods, on_delete=models.SET_NULL,null=True)
+    reporting_period_id = models.ForeignKey(ReportingPeriods, on_delete=models.SET_NULL, null=True)
     name = models.CharField('Наименование', max_length=250)
-    # не знаю как называть
     commentable = models.IntegerField('commentable')
-
 
     def __str__(self):
         return f'{self.name}'
@@ -178,20 +177,20 @@ class Translations(models.Model):
 class Users(models.Model):
     created_at = models.DateTimeField('Время создание ', blank=True)
     updated_at = models.DateTimeField('Время изменения', blank=True)
-    objectguid = models.CharField("objectguid", max_length=250,null=True)
+    objectguid = models.CharField("objectguid", max_length=250, null=True)
     name = models.CharField("Наименование", max_length=250)
-    avatar = models.CharField("avatar", max_length=250,null=True)
-    position_id = models.ForeignKey(Positions, on_delete=models.SET_NULL,null=True)
+    avatar = models.CharField("avatar", max_length=250, null=True)
+    position_id = models.ForeignKey(Positions, on_delete=models.SET_NULL, null=True)
     password = models.CharField("Пароль", max_length=250)
     remember_token = models.IntegerField("remember_token")
-    settings = models.TextField("Настройки",null=True)
+    settings = models.TextField("Настройки", null=True)
     username = models.CharField("Имя пользователя", max_length=250)
-    inspector_id = models.ForeignKey(Inspectors,on_delete=models.SET_NULL,null=True)
-    department_id = models.ForeignKey(Departments, on_delete=models.SET_NULL,null=True)
-    orcid = models.CharField("orcid", max_length=250,null=True)
-    worsrid = models.CharField("worsrid", max_length=250,null=True)
-    said = models.CharField("Оценка", max_length=250,null=True)
-    spin = models.CharField('spin', max_length=250,null=True)
+    inspector_id = models.ForeignKey(Inspectors, on_delete=models.SET_NULL, null=True)
+    department_id = models.ForeignKey(Departments, on_delete=models.SET_NULL, null=True)
+    orcid = models.CharField("orcid", max_length=250, null=True)
+    worsrid = models.CharField("worsrid", max_length=250, null=True)
+    said = models.CharField("Оценка", max_length=250, null=True)
+    spin = models.CharField('spin', max_length=250, null=True)
 
     def __str__(self):
         return f'{self.name}'
@@ -208,13 +207,13 @@ class UserRoles(models.Model):
 class Values(models.Model):
     created_at = models.DateTimeField('Время создание ', blank=True)
     updated_at = models.DateTimeField('Время изменения', blank=True)
-    field_id = models.ForeignKey(Fields, on_delete=models.SET_NULL,null=True)
-    user_id = models.ForeignKey(Users, on_delete=models.SET_NULL,null=True)
-    value = models.TextField("Значение",null=True)
+    field_id = models.ForeignKey(Fields, on_delete=models.SET_NULL, null=True)
+    user_id = models.ForeignKey(Users, on_delete=models.SET_NULL, null=True)
+    value = models.TextField("Значение", null=True)
     lock = models.BooleanField("Допуск")
-    comment = models.TextField("Комментарий",null=True)
+    comment = models.TextField("Комментарий", null=True)
     visible = models.BooleanField("Видимый")
-    category_id = models.ForeignKey(Categories,on_delete=models.SET_NULL,null=True)
+    category_id = models.ForeignKey(Categories, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return f'{self.user_id}'
@@ -226,8 +225,8 @@ class Files(models.Model):
     path = models.CharField('Путь', max_length=250)
     name = models.TextField('Наименование', max_length=250)
     description = models.TextField('Описание', max_length=250)
-    file_category_id = models.ForeignKey(FilesCatigories, on_delete=models.SET_NULL,null=True)
-    user_id = models.ForeignKey(Users, on_delete=models.SET_NULL,null=True)
+    file_category_id = models.ForeignKey(FilesCatigories, on_delete=models.SET_NULL, null=True)
+    user_id = models.ForeignKey(Users, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return f'{self.name}'
